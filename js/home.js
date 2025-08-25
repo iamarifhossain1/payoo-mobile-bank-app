99409b0ae8f9add6b92800fa1c86aff12ca90007
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-08-25 13:37:50 +0600

    add money

---
+++ js/home.js
@@ -0,0 +1,31 @@
+const validPin = 1234
+
+document.getElementById('add-money-btn')
+.addEventListener('click', function(event){
+    event.preventDefault();
+    const selectBank = document.getElementById('select-bank').value;
+    const bankAccount = document.getElementById('bank-account').value;
+    const addAmount = parseInt(document.getElementById('add-amount').value);
+    const pinNumber = parseInt(document.getElementById('pin-number').value);
+    const balance = parseInt(document.getElementById('balance').innerText);
+
+    if(bankAccount.length < 11) {
+        alert('Please provide a valid bank account number');
+        return;
+    }
+
+    if(addAmount < 100) {
+        alert('Minimum Transaction 100 Taka')
+        
+    }
+
+    if(pinNumber !== validPin) {
+        alert('Please provide a valid pin number');
+        return;
+    }
+    
+    const addAmountBalance = addAmount + balance;
+    const newBalance = document.getElementById('balance').innerText = addAmountBalance;
+    
+    
+})
